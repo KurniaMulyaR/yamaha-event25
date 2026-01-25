ec6aafa3e4bf2bc779b777851485b34bbbb7726d
KurniaMulyaR <kurniamulyarizqi1006@gmail.com>
2026-01-25 14:32:48 +0700

    penen

---
+++ database/migrations/2026_01_25_073208_add_rekomendasi_to_table_list_pesanan.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRekomendasiToTableListPesanan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('list_pesanan', function (Blueprint $table) {
+            $table->text('snaptoken');
+            $table->text('orderid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('list_pesanan', function (Blueprint $table) {
+            $table->dropColumn('snaptoken');
+            $table->dropColumn('orderid');
+        });
+    }
+}
